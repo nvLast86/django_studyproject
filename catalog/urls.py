eecc6c7aa9014856923b5a730994227c95d93bd3
nvLast86 <n.lastenko@gmail.com>
2023-05-04 12:45:29 +0600

    app's urls.py has been changed

--- catalog/urls.py
+++ catalog/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import contacts, home
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', contacts),
+    path('', home)
 ]
