e41927686b30c018c4c534db97131d3f965c08f0
nvLast86 <n.lastenko@gmail.com>
2023-05-18 10:57:50 +0600

    created_at field has been deleted using migrations

--- catalog/models.py
+++ catalog/models.py
@@ -24,7 +24,6 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='название')
     description = models.TextField(verbose_name='описание', **NULLABLE)
-    created_at = models.DateField(verbose_name='создано', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}, {self.description}'
