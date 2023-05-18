98218119a3bd80e89ef5640b54b41bc3f8cf0931
nvLast86 <n.lastenko@gmail.com>
2023-05-18 10:53:31 +0600

    created_at field has been added and migrated to category model

--- catalog/models.py
+++ catalog/models.py
@@ -7,7 +7,7 @@ NULLABLE = {'blank': True, 'null': True}
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='название')
     description = models.TextField(verbose_name='описание', **NULLABLE)
-    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    image_preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
     category = models.CharField(max_length=100, verbose_name='категория')
     price = models.FloatField(verbose_name='цена')
     creation_date = models.DateField(verbose_name='дата создания')
@@ -24,6 +24,7 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='название')
     description = models.TextField(verbose_name='описание', **NULLABLE)
+    created_at = models.DateField(verbose_name='создано', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}, {self.description}'
